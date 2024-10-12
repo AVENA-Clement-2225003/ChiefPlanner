5b262de0155d4518d679eeeda4ec7f0f30b397e8
Michael KERGUELEN <clement@antranet.fr>
2024-10-12 22:24:47 +0200

    Avancées

---
+++ app/Models/Quantitees.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quantitees extends Model
+{
+    use HasFactory;
+    protected $table = 'quantitees';
+    public $timestamps = false;
+}
