8033227a23c1a327b44afe101fb8becc186355b1
Michael <clement@antranet.fr>
2025-05-09 13:28:33 +0200

    pas mal on continue encore un peu

--- resources/views/preferences.blade.php
+++ resources/views/preferences.blade.php
@@ -3,30 +3,34 @@
 @section('title', 'ChiefPlanner - Préférences')
 
 @section('content')
-    <div class="weekHolder">
-        <p class="widgetTitle">Jours sélectionnés</p>
-        <form action="/preferences/update" method="post">
-            @csrf
-            <div class="weekSelection">
-            @foreach($daylist as $dayName => $day)
-                <div class="daySelection">
-                    <p>{{ $dayName }}</p>
-                    <label> <!--#290404 Il faut faire la pose de la classe par le JS car il faut que le check box soit selectionné de manière concrete par le navigateur comme ça quand je clique ça fait apparaitre, disparaitre le vert ou pas donc il faut aussi revoir mon css lié au day selected-->
-                        <input type="checkbox"
-                               name="schedule[{{ $dayName }}][morning]"
-                               @if($day['morning'] == 1) checked @endif>
-                        <span class="day custom-checkbox"></span>
-                    </label>
-                    <label>
-                        <input type="checkbox"
-                               name="schedule[{{ $dayName }}][afternoon]"
-                               @if($day['afternoon'] == 1) checked @endif>
-                        <span class="day custom-checkbox"></span>
-                    </label>
+    <div class="widgetHolder">
+        <div class="widget preferences-widget">
+            <h2 class="widgetTitle">Jours sélectionnés</h2>
+            <form action="/preferences/update" method="post" class="preferences-form">
+                @csrf
+                <div class="weekSelection">
+                @foreach($daylist as $dayName => $day)
+                    <div class="daySelection">
+                        <p>{{ $dayName }}</p>
+                        <label class="day-label">
+                            <input type="checkbox"
+                                   name="schedule[{{ $dayName }}][morning]"
+                                   @if($day['morning'] == 1) checked @endif>
+                            <span class="day custom-checkbox"></span>
+                        </label>
+                        <label class="day-label">
+                            <input type="checkbox"
+                                   name="schedule[{{ $dayName }}][afternoon]"
+                                   @if($day['afternoon'] == 1) checked @endif>
+                            <span class="day custom-checkbox"></span>
+                        </label>
+                    </div>
+                @endforeach
                 </div>
-            @endforeach
-            </div>
-            <input type="Submit" value="Mettre à jour">
-        </form>
+                <div class="form-actions">
+                    <button type="submit" class="btn">Mettre à jour</button>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
